901192af138e49e2499a73d518654dc66128ece2
coody <coody0829@gmail.com>
2015-06-03 20:19:20 +0800

    [GW2_WebSite]
    1. 加入測試案例專案，方便獨立測試 WebSite 的相關功能

---
+++ WebSiteForGW2Test/WebSiteForGW2Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebSiteForGW2Test
+//
+//  Created by CoodyChou on 2015/6/3.
+//  Copyright (c) 2015年 CoodyChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
